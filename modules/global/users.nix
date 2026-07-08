8a6b16710c1781766407899dbd85714e66655b73
waxkb <maxwellr2028@gmail.com>
2026-07-08 16:58:20 -0700

    e

--- modules/global/users.nix
+++ modules/global/users.nix
@@ -21,4 +21,11 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGh6KWt6dNi46lrhhzfS54012/UPitFzhRWlDDNqb+To maxwellr2028@gmail.com"
     ];
   };
+
+  users.users.root = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGh6KWt6dNi46lrhhzfS54012/UPitFzhRWlDDNqb+To maxwellr2028@gmail.com"
+    ];
+    shell = pkgs.zsh;
+  };
 }
